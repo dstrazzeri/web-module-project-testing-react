26d7013f6d34a0a674b8284d8dfbae44617eb5ea
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2021-12-13 18:21:29 -0500

    wrote out tests for show & episode

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -5,11 +5,31 @@ import userEvent from '@testing-library/user-event';
 
 import Display from './../Display';
 
+const testShow = {
+    name: "test show",
+    summary: "test summary",
+    seasons: [
+        {
+            id: 0,
+            name: "Season 1",
+            episodes: []
+        },
+        {
+            id: 1,
+            name: "Season 2",
+            episodes: []
+        }
+    ]
+}
 
 test('renders without errors with no props', ()=>{
-
+    render(<Display />)
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test('renders Show component when the button is clicked ', ()=>{
+    render(<Display />)
+    const button = screen.getByRole("button");
+    userEvent.clickButton();
+});
 
 test('renders show season options matching your data when the button is clicked', ()=>{});
